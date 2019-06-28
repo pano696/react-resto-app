76d9af218b56f251b96baffd5bc4200c2e0c16df
Мурзагалиев Павел Серикович <murzagaliev@obalt.baltimore.ru>
2019-06-28 15:17:17 +0300

    Lesson 14 Normal

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,17 +2,22 @@ import React from 'react';
 import {MainPage, CartPage} from '../pages';
 import AppHeader from '../app-header';
 import WithRestoService from '../hoc';
+import {BrowserRouter as Router,  Route, Switch} from 'react-router-dom';
 
 import Background from './food-bg.jpg';
 
 const App = ({RestoService}) => {
   console.log(RestoService.getMenuItems())
     return (
-        <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
+        <Router>
+          <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
             <AppHeader total={50}/>
-            <MainPage/>
-            <CartPage/>
-        </div>
+            <Switch>
+              <Route path='/' exact component={MainPage} />
+              <Route path='/cart' exact component={CartPage} />
+            </Switch>
+          </div>
+        </Router>
     )
 }
 
--- src/services/resto-service.js
+++ src/services/resto-service.js
@@ -1,5 +1,21 @@
 export default class RestoService {
-  getMenuItems() {
-    return [];
+
+  constructor() {
+    this._baseUrl = 'http://localhost:3000'
+  }
+
+  getData = async (url) => {
+    const response = await fetch(`${this._baseUrl}${url}`)
+    if (!response.ok) throw new Error(`Could not fetch ${url}, status: ${response.status}`);
+
+    return await response.json()
+
+  }
+
+  getMenuItems = async () => {
+    const menuItems = await this.getData('/menu')
+    console.log(menuItems);
+
+    return menuItems;
   }
 }
